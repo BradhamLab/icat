6f8d09a774688948f3e98b4b671ae7373f89f9d6
dakota-hawkins <dakotahawkins0110@gmail.com>
2019-05-21 12:35:03 -0400

    Added baseline control performance to output.

--- src/evaluation.py
+++ src/evaluation.py
@@ -24,33 +24,45 @@ def evaluate_icat(control, treated, label_col, icat_kws):
         n_neighbors = icat_kws['neighbor_kws']['n_neighbors']
     except KeyError:
         n_neighbors = 15
+    sc.pp.neighbors(control, n_neighbors=n_neighbors)
+    sc.tl.umap(control, min_dist=0.0)
+    sc.tl.louvain(control, key_added='control.louvain')
+    control_only = performance(control, 'Population', 'control.louvain')
+    for key in list(control_only.keys()):
+        value = control_only.pop(key)
+        control_only['base.' + key] = value
     sc.pp.neighbors(combined, n_neighbors=n_neighbors)
     sc.tl.umap(combined, min_dist=0.0)
     sc.tl.louvain(combined)
     icat = models.icat(**icat_kws)
     icat_clustered = icat.cluster(control, treated)
     sc.tl.louvain(icat_clustered, key_added='ncfs.louvain')
-    performance = []
+    measures = []
     
     # mutual information, homogeneity score, completeness score
     # rand, fowlkes mallows
     for data, col in zip([combined, icat_clustered, icat_clustered],
                          ['louvain', 'ncfs.louvain', 'sslouvain']):
-        known = data.obs[label_col].astype(str)
-        pred = data.obs[col].astype(str)
-        mi = metrics.adjusted_mutual_info_score(known, pred)
-        homog = metrics.homogeneity_score(known, pred)
-        comp = metrics.completeness_score(known, pred)
-        ar = metrics.adjusted_rand_score(known, pred)
-        fm = metrics.fowlkes_mallows_score(known, pred)
-        performance.append({'method': col,
-                            'adjusted.mutual.info': mi,
-                            'homogeneity': homog,
-                            'completeness': comp,
-                            'adjusted.rand': ar,
-                            'fowlkes.mallows': fm})
-    return performance
+        split_perf = performance(data, 'Population', col)
+        split_perf['method'] = col
+        measures.append(dict(split_perf, **control_only))
 
+    return measures
+
+def performance(adata, true_col, pred_col):
+    known = adata.obs[true_col].astype(str)
+    pred = adata.obs[pred_col].astype(str)
+    mi = metrics.adjusted_mutual_info_score(known, pred)
+    homog = metrics.homogeneity_score(known, pred)
+    comp = metrics.completeness_score(known, pred)
+    ar = metrics.adjusted_rand_score(known, pred)
+    fm = metrics.fowlkes_mallows_score(known, pred)
+    measures = {'adjusted.mutual.info': mi,
+                'homogeneity': homog,
+                'completeness': comp,
+                'adjusted.rand': ar,
+                'fowlkes.mallows': fm}
+    return measures
 
 if __name__ == '__main__':
     data_dir = '../data/processed/simulated'
@@ -75,7 +87,7 @@ if __name__ == '__main__':
         perturb_str = snakemake.params['prtb_str']
     fit_data = pd.read_csv(control_csv, index_col=0)
     out_list = []
-    for exp in fit_data.index.values[:2]:
+    for exp in fit_data.index.values:
         control_fn = os.path.join(data_dir, exp)
         perturb_fn = os.path.join(data_dir, 
                                   exp.replace(control_str, perturb_str))
@@ -89,7 +101,7 @@ if __name__ == '__main__':
         df = pd.DataFrame(perf)
         df['Experiment'] = exp_re.search(exp).group()
         df['Sim'] = sim_re.search(exp).group()
-        df['Rep'] = sim_re.search(exp).group()
+        df['Rep'] = rep_re.search(exp).group()
         out_list.append(df)
     out_df = pd.concat(out_list, axis=0).reset_index(drop=True)
     out_df.to_csv(out_csv)
