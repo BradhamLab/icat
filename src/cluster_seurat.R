d133a203d14d05c4c59740940b48e7f9a5ffec78
dakota-hawkins <dakotahawkins0110@gmail.com>
2019-05-24 21:08:17 -0400

    Added Seurat comparisons.

---
+++ src/cluster_seurat.R
@@ -0,0 +1,62 @@
+library(Seurat)
+
+create_seurat <- function(X, obs) {
+  X_data = as.data.frame(t(read.csv(X, header=FALSE)))
+  obs_data = read.csv(obs, row.names=1, check.names=FALSE, stringsAsFactors=FALSE)
+  # force to population
+  obs_data$Population <- as.factor(obs_data$Population)
+  row.names(obs_data) <- colnames(X_data)
+  data <- Seurat::CreateSeuratObject(raw.data=X_data, meta.data=obs_data)
+  data <- Seurat::NormalizeData(data)
+  data <- Seurat::ScaleData(data)
+  data <- Seurat::FindVariableGenes(object=data, do.plot=FALSE)
+}
+
+rename_cells <- function(seurat_obj, prefix) {
+  ids <- sapply(1:ncol(seurat_obj@raw.data), function(i) paste0(prefix, as.character(i)))
+  return(Seurat::RenameCells(seurat_obj, new.names=ids))
+}
+
+cluster_across_treatments <- function(ctrl, prtb) {
+  # rename cells, add treatment column
+  ctrl <- rename_cells(ctrl, 'ctrl')
+  ctrl@meta.data$treatment <- rep('ctrl', ncol(ctrl@raw.data))
+  prtb <- rename_cells(prtb, 'prtb')
+  prtb@meta.data$treatment <- rep('prtb', ncol(prtb@raw.data))
+  combined <- Seurat::RunCCA(object=ctrl, object2=prtb)
+  combined <- Seurat::RunPCA(combined)
+  combined <- Seurat::CalcVarExpRatio(object=combined, reduction.type='pca',
+                                      grouping.var='treatment')
+  kept <- Seurat::SubsetData(combined, subset.name='var.ratio.pca',
+                             accept.low=0.5)
+  discarded <- Seurat::SubsetData(combined, subset.name='var.ratio.pca',
+                                  accept.high=0.5)
+  kept <- Seurat::AlignSubspace(kept, reduction.type='cca',
+                                grouping.var='treatment', dims.align=1:20)
+  kept <- Seurat::FindClusters(kept, reduction.type='cca.aligned', dims.use=1:20)
+  metadata <- combined@meta.data
+  metadata$cluster <- NULL
+  combined@meta.data[row.names(kept@meta.data), 'cluster'] <- sapply(kept@meta.data$res.0.8,
+                                                            function(x) paste0('aligned', as.character(x)))
+  combined@meta.data[row.names(discarded@meta.data), 'cluster'] <- rep('unknown', nrow(discarded@meta.data))
+  combined <- Seurat::RunUMAP(combined)
+  combined@meta.data <- cbind(combined@meta.data, combined@dr$umap@cell.embeddings)
+  return(combined@meta.data)
+}
+
+main <- function(X_ctrl, obs_ctrl, X_prtb, obs_prtb, out_csv) {
+  ctrl <- create_seurat(X_ctrl, obs_ctrl)
+  prtb <- create_seurat(X_prtb, obs_prtb)
+  clustered <- cluster_across_treatments(ctrl, prtb)
+  write.csv(clustered, out_csv)
+}
+
+if (exists('snakemake')) {
+  main(snakemake@input[['X_ctrl']],
+       snakemake@input[['obs_ctrl']],
+       snakemake@input[['X_prtb']],
+       snakemake@input[['obs_prtb']],
+       snakemake@output[['csv']])
+}
+
+
