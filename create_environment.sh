b006ed3160a23d4bd387c9c4126334e83de4d161
Dakota Hawkins <dakotahawkins0110@gmail.com>
2019-07-17 16:18:37 -0400

    fixed anndata issue.

--- create_environment.sh
+++ create_environment.sh
@@ -1,2 +1,2 @@
 conda create --name icat --file spec-file.txt
-pip install anndata==0.619 h5py==2.90 natsort==6.0.0 scanpy==1.4.3 tqdm==4.32.1 umap-learn==0.3.8
+pip install anndata==0.6.19 h5py==2.90 natsort==6.0.0 scanpy==1.4.3 tqdm==4.32.1 umap-learn==0.3.8
