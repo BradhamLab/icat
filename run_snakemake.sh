0b4c12a3903c9942e04ea16991fceb72651c2934
dakota-hawkins <dyh0110@bu.edu>
2019-10-22 10:42:20 -0400

    flags for gcc libs

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -6,4 +6,5 @@
 export PYTHONPATH="{PYTHONPATH}:/projectnb/bradham/PythonModules"
 export R_LIBS="{R_LIBS}:/projectnb/bradham/RPackages"
 source activate icat
-snakemake --cluster 'qsub -v PYTHONPATH=/projectnb/bradham/PythonModules -P bradham -pe omp 3' --jobs 100
+snakemake --cluster \
+'qsub -v PYTHONPATH=/projectnb/bradham/PythonModules -v LD_LIBRARY_PATH=/share/pkg.7/gcc/8.1.0/install/lib64:/share/pkg.7/gcc/8.1.0/install/lib -P bradham -pe omp 3' --jobs 100
