5a3c6d9ddcdb6d3a0f233a04f56dc2163743ad26
dakota-hawkins <dakotahawkins0110@gmail.com>
2019-10-30 23:37:40 -0400

    now plotting regression plots.

--- src/plot_metric_trends.py
+++ src/plot_metric_trends.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import pandas as pd
 import numpy as np
@@ -7,6 +8,10 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 
 from icat.src import plot_performance
+
+def experiment_match(exp_id, index):
+    regex = re.compile(exp_id)
+    return any([regex.search(x) is not None for x in index])
 
 if __name__ == '__main__':
     try:
@@ -20,19 +25,28 @@ if __name__ == '__main__':
         feature = snakemake.params['sim_feature']
         subset = simulations[simulations.apply(lambda x: exp_id in x.name,
                                                axis=1)]
-        results['ExpID'] = results.apply(lambda x: '{}.{}'.format(x.Experiment,
-                                                                x.Perturbation),
-                                        axis=1)
+        if feature == 'dropout':
+            results['ExpID'] = results.apply(lambda x: '{}.{}{}{}'.format(
+                                                                x.Experiment,
+                                                                x.Perturbation,
+                                                                x.Sim,
+                                                                x.Rep),
+                                             axis=1)
+        else:
+            # match ExpID column in results Dataframe to subset index values
+            results['ExpID'] = results.apply(lambda x: '{}.{}'.format(x.Experiment,
+                                                                    x.Perturbation),
+                                            axis=1)
         results = results[results.apply(lambda x: x.ExpID in subset.index,
                                         axis=1)]
         results.loc[results.index, feature] = results.apply(lambda x:\
                                                    subset.loc[x.ExpID, feature],
                                                    axis=1)
-        results['ExpID'] = results.apply(lambda x: "{}{}{}".format(
-                                                   x['Experiment'],
-                                                   x['Perturbation'],
-                                                   x['Sim']),
-                                         axis=1)
+        results.loc[results.index, 'ExpID'] = results.apply(lambda x: "{}{}{}"\
+                                                     .format(x['Experiment'],
+                                                             x['Perturbation'],
+                                                             x['Sim']),
+                                                            axis=1)
         sim_mean = results.groupby(['ExpID', 'method']).mean()
         sim_mean['Method'] = sim_mean.index.get_level_values('method')
         sim_mean['Method'] = sim_mean['Method'].apply(lambda x:\
@@ -40,14 +54,11 @@ if __name__ == '__main__':
         sim_mean.rename(columns=plot_performance.metric_dictionary,
                         inplace=True)
         to_metric = {v:k for k, v in plot_performance.metric_dictionary.items()}
+        sim_mean[feature] = sim_mean[feature] * 100
         for y in plot_performance.metric_dictionary.values():
-            ax = plot_performance.trendplot(sim_mean, x=feature,
-                                            y=y, hue='Method', style='Method')
-            xticks, __ = plt.xticks()
-            xlabels = ["{:0.1f}".format(x*100) for x in xticks]
-            plt.xlabel(snakemake.params['xlabel'], labelpad=5)
-            plt.xticks(xticks, xlabels)
-            plt.tight_layout()
+            plot_performance.trendplot(sim_mean, x=feature,
+                                       y=y, hue='Method',
+                                       xlabel=snakemake.params['xlabel'])
             plt.savefig(os.path.join(snakemake.params['plotdir'],
                         to_metric[y].replace('.', '-') + '.svg'))
             plot_performance.close_plot()
