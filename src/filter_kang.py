ef5ad46ab9f6ef0a593449ae0835e4f3cc6de1b7
dakota-hawkins <dyh0110@bu.edu>
2019-11-06 10:45:07 -0500

    there was a change i guess.

--- src/filter_kang.py
+++ src/filter_kang.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
         adata = sc.AnnData(X=np.loadtxt(snakemake.input['X'], delimiter=','),
                            obs=pd.read_csv(snakemake.input['obs'], index_col=0),
                            var=pd.read_csv(snakemake.input['var'], index_col=0))
-        obs.index.name = 'cell-barcode'
-        obs.rename(columns={'cell.1': 'cell'}, inplace=True)
+        adata.obs.index.name = 'cell-barcode'
+        adata.obs.rename(columns={'cell.1': 'cell'}, inplace=True)
         sc.pp.filter_genes(adata, min_cells=3)
-        adata.write_csvs(dirnames=snakemake.params['countdir'], skip_data=False)
+        adata.write_csvs(dirname=snakemake.params['countdir'], skip_data=False)
         # normalize data
         sc.pp.normalize_total(adata)
         # log transform counts to detected highly variable genes
