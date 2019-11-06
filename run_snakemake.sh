810882e4525cd9e1844bc779fbf6d0906183d0e6
dakota-hawkins <dyh0110@bu.edu>
2019-11-06 14:10:42 -0500

    modified to work with cluster configuration file.

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -l
-#$ -N scanorama_test
+#$ -N seurat_test
 #$ -M dyh0110@bu.edu
 #$ -m eas
 
@@ -7,4 +7,4 @@ export PYTHONPATH="{$PYTHONPATH}:/projectnb/bradham/PythonModules"
 export R_LIBS="{$R_LIBS}:/projectnb/bradham/RPackages"
 source activate icat
 snakemake --cluster \
-'qsub -v PYTHONPATH=/projectnb/bradham/PythonModules -v LD_LIBRARY_PATH=/share/pkg.7/gcc/8.1.0/install/lib64:/share/pkg.7/gcc/8.1.0/install/lib -P bradham -pe omp 4 -e snake_error -o snake_out' --jobs 100 --latency-wait 30 --cluster-config cluster-config.json
+'qsub -v PYTHONPATH={cluster.python} -v LD_LIBRARY_PATH={cluster.gcc} -P {cluster.project} -pe omp 4 -e {cluster.error} -o {cluster.out} -l cpu_arch={cluster.cpus}' --jobs 100 --latency-wait 30 --cluster-config cluster.json
