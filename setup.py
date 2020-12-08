2999aa82fca11a994663da997065583bcf6b4a0e
dakota-hawkins <dakotahawkins0110@gmail.com>
2020-12-08 14:31:41 -0500

    one-stop shop for version info.

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup
+import re
+
+# taken from https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+VERSIONFILE="icat/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 setup(name='icat',
-      version='0.1',
+      version=verstr,
       description="Identify cell-types across treatments in single-cell RNA sequencing data",
       url='https://github.com/dakota-hawkins/icat',
       author='Dakota Y. Hawkins',
